a647543c07bdfe4ff5b55fbaed8743358c730e02
Ayham Mamoun <ayhamaboualfadl@gmail.com>
2019-01-12 11:23:14 +0300

    Initial Relase!

---
+++ CMakeFiles/clox.dir/DependInfo.cmake
@@ -0,0 +1,25 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/home/ayham-mamoun/Desktop/git repos/clox/clox/chunk.c" "/home/ayham-mamoun/Desktop/git repos/clox/CMakeFiles/clox.dir/clox/chunk.c.o"
+  "/home/ayham-mamoun/Desktop/git repos/clox/clox/debug.c" "/home/ayham-mamoun/Desktop/git repos/clox/CMakeFiles/clox.dir/clox/debug.c.o"
+  "/home/ayham-mamoun/Desktop/git repos/clox/clox/main.c" "/home/ayham-mamoun/Desktop/git repos/clox/CMakeFiles/clox.dir/clox/main.c.o"
+  "/home/ayham-mamoun/Desktop/git repos/clox/clox/memory.c" "/home/ayham-mamoun/Desktop/git repos/clox/CMakeFiles/clox.dir/clox/memory.c.o"
+  "/home/ayham-mamoun/Desktop/git repos/clox/clox/value.c" "/home/ayham-mamoun/Desktop/git repos/clox/CMakeFiles/clox.dir/clox/value.c.o"
+  "/home/ayham-mamoun/Desktop/git repos/clox/clox/vm.c" "/home/ayham-mamoun/Desktop/git repos/clox/CMakeFiles/clox.dir/clox/vm.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ CMakeFiles/clox.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/clox.dir/clox/chunk.c.o"
+  "CMakeFiles/clox.dir/clox/debug.c.o"
+  "CMakeFiles/clox.dir/clox/main.c.o"
+  "CMakeFiles/clox.dir/clox/memory.c.o"
+  "CMakeFiles/clox.dir/clox/value.c.o"
+  "CMakeFiles/clox.dir/clox/vm.c.o"
+  "bin/clox.pdb"
+  "bin/clox"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/clox.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
